9e6b8692cd0451e8907bfea189b4a37a072372c9
yczxxs1 <junbaizhang@outlook.com>
2019-01-13 02:38:31 +0800

    我的查询，条件查询，删除优化

--- house-rental/src/main/resources/static/js/main.js
+++ house-rental/src/main/resources/static/js/main.js
@@ -21,204 +21,143 @@ layui.use('form', function () {
             data: rentInfoData,
             dataType: "json",
             success: function (msg) {
-                if (msg.status == 0) {
+                if (msg.status === 0) {
                     $(location).attr('href', 'list.html');
-                    layer.msg("发布成功");
+                    layer.msg("发布成功！");
 
                 } else {
-                    layer.msg("发布失败");
+                    layer.msg("发布失败！");
                 }
 
             },
             error: function (msg) {
-                layer.msg("发布失败");
+                layer.msg("发布失败！");
             }
         });
 
         return false;
     });
 
-
-    //监听提交
-    form.on('submit(formQuery)', function (data) {
-        //layer.msg(JSON.stringify(data.field));
-        $("#LAY_demo1").html("");
-        var queryData = data.field;
-        $.ajax({
-            url: "/rentalInfo/list",
-            type: "GET",
-            data: queryData,
-            dataType: "json",
-            success: function (res) {
-                var arr=res.data.list;
-
-                for ( var i = 0; i <arr.length; i++){
-                    $("#LAY_demo1").append(
-                        '<li>' +
-
-                        '<div class="layui-row">' +
-                        '<div class="layui-col-xs3">' +
-                        '<div class="layui-form-mid"><img class="main-list-img" src="' + arr[i].houseImgs + '" id="demo1">' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="layui-col-xs9">' +
-
-                        '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                        '<div class="layui-inline">' +
-                        '<div class="layui-form-mid">' +
-                        '<span class="main-title">' + arr[i].houseName + '</span>' +
-                        '</div>' +
-                        '</div>' +
-                        '<div class="main-list-right">' +
-                        '<span class="main-money">' + arr[i].houseRent + '元/月</span>' +
-                        '</div>' +
-                        '</div>' +
-
-                        '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                        '<div class="layui-inline">' +
-                        '<div class="layui-form-mid">' +
-                        '<span>户型:' + arr[i].houseType + '</span>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-
-                        '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                        '<div class="layui-inline">' +
-                        '<div class="layui-form-mid">' +
-                        '<span>位置:' + arr[i].houseLocation + '</span>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-
-                        '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                        '<div class="layui-inline">' +
-                        '<div class="layui-form-mid">' +
-                        '<span>联系身份:' + arr[i].houseContactIdentity + '</span>' +
-                        '</div>' +
-                        '</div>' +
-
-                        '<div class="layui-inline" style="float: right">' +
-                        '<button class="layui-btn layui-btn-primary layui-btn-xs" id="edit"><i class="layui-icon layui-icon-edit">更新</i>' +
-                        '</button>' +
-                        '<button class="layui-btn layui-btn-primary layui-btn-xs" id="remove" onclick="remove(this)">' +
-                        '<input type="hidden" id="rentalInfoId" value="' + arr[i].rentalInfoId + '">' +
-                        '<i class="layui-icon layui-icon-delete">删除</i>' +
-                        '</button>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-                        '</div>' +
-
-                        '</li>'
-
-                    );
-                }
-
-
-
-
-            }
-        });
-
-        return false;
-    });
 });
 
 //flow
 layui.use('flow', function () {
     var flow = layui.flow;
 
-    flow.load({
-        elem: '#LAY_demo1' //流加载容器
-        , done: function (page, next) { //执行下一页的回调
-            var lis = [];
-            //以jQuery的Ajax请求为例，请求下一页数据（注意：page是从2开始返回）
-                //"rentalInfoUserId":null,
-            var formData={
-                "page":page,
-                "bedroomNum": document.getElementById("bedroomNum").value,
-                "houseContactIdentity":document.getElementById("houseContactIdentity").value,
-                "maxRent":document.getElementById("maxRent").value,
-                "minRent":document.getElementById("minRent").value
-            };
+    var pullDate = function (options) {
+        $.extend({}, {
+            "page": 1
+        });
+        $('.layadmin-tabsbody-item').off('scroll');
+        flow.load({
+            elem: '#LAY_demo1' //流加载容器
+            , done: function (page, next) { //执行下一页的回调
+                var lis = [];
+                var formData = {
+                    "page": page,
+                    "bedroomNum": document.getElementById("bedroomNum").value,
+                    "houseContactIdentity": document.getElementById("houseContactIdentity").value,
+                    "maxRent": document.getElementById("maxRent").value,
+                    "minRent": document.getElementById("minRent").value,
+                    "rentalInfoUserId": options
+                };
+                $.ajax({
+                    url: "/rentalInfo/list",
+                    type: "GET",
+                    dataType: "json",
+                    data: formData,
+                    success: function (res) {
+                        layui.each(res.data.list, function (index, item) {
+                            lis.push(
+                                '<li>' +
 
-            $.ajax({
-                url: "/rentalInfo/list",
-                type: "GET",
-                dataType: "json",
-                data:formData,
-                success: function (res) {
-                    layui.each(res.data.list, function (index, item) {
-                        lis.push(
-                            '<li>' +
+                                '<div class="layui-row">' +
+                                '<div class="layui-col-xs3">' +
+                                '<div class="layui-form-mid"><img class="main-list-img" src="' + item.houseImgs + '" id="demo1">' +
+                                '</div>' +
+                                '</div>' +
+                                '<div class="layui-col-xs9">' +
 
-                            '<div class="layui-row">' +
-                            '<div class="layui-col-xs3">' +
-                            '<div class="layui-form-mid"><img class="main-list-img" src="' + item.houseImgs + '" id="demo1">' +
-                            '</div>' +
-                            '</div>' +
-                            '<div class="layui-col-xs9">' +
+                                '<div class="layui-form-item" style="margin-bottom: 1px">' +
+                                '<div class="layui-inline">' +
+                                '<div class="layui-form-mid">' +
+                                '<span class="main-title">' + item.houseName + '</span>' +
+                                '</div>' +
+                                '</div>' +
+                                '<div class="main-list-right">' +
+                                '<span class="main-money">' + item.houseRent + '元/月</span>' +
+                                '</div>' +
+                                '</div>' +
 
-                            '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                            '<div class="layui-inline">' +
-                            '<div class="layui-form-mid">' +
-                            '<span class="main-title">' + item.houseName + '</span>' +
-                            '</div>' +
-                            '</div>' +
-                            '<div class="main-list-right">' +
-                            '<span class="main-money">' + item.houseRent + '元/月</span>' +
-                            '</div>' +
-                            '</div>' +
+                                '<div class="layui-form-item" style="margin-bottom: 1px">' +
+                                '<div class="layui-inline">' +
+                                '<div class="layui-form-mid">' +
+                                '<span>户型:' + item.houseType + '</span>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
 
-                            '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                            '<div class="layui-inline">' +
-                            '<div class="layui-form-mid">' +
-                            '<span>户型:' + item.houseType + '</span>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
+                                '<div class="layui-form-item" style="margin-bottom: 1px">' +
+                                '<div class="layui-inline">' +
+                                '<div class="layui-form-mid">' +
+                                '<span>位置:' + item.houseLocation + '</span>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
 
-                            '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                            '<div class="layui-inline">' +
-                            '<div class="layui-form-mid">' +
-                            '<span>位置:' + item.houseLocation + '</span>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
+                                '<div class="layui-form-item" style="margin-bottom: 1px">' +
+                                '<div class="layui-inline">' +
+                                '<div class="layui-form-mid">' +
+                                '<span>联系人身份:' + item.houseContactIdentity + '</span>' +
+                                '</div>' +
+                                '</div>' +
 
-                            '<div class="layui-form-item" style="margin-bottom: 1px">' +
-                            '<div class="layui-inline">' +
-                            '<div class="layui-form-mid">' +
-                            '<span>联系身份:' + item.houseContactIdentity + '</span>' +
-                            '</div>' +
-                            '</div>' +
+                                '<div class="layui-inline" style="float: right">' +
+                                '<button class="layui-btn layui-btn-primary layui-btn-xs" id="edit"><i class="layui-icon layui-icon-edit">更新</i>' +
+                                '</button>' +
+                                '<button class="layui-btn layui-btn-primary layui-btn-xs" id="remove" onclick="remove(this)">' +
+                                '<input type="hidden" id="rentalInfoId" value="' + item.rentalInfoId + '">' +
+                                '<i class="layui-icon layui-icon-delete">删除</i>' +
+                                '</button>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
 
-                            '<div class="layui-inline" style="float: right">' +
-                            '<button class="layui-btn layui-btn-primary layui-btn-xs" id="edit"><i class="layui-icon layui-icon-edit">更新</i>' +
-                            '</button>' +
-                            '<button class="layui-btn layui-btn-primary layui-btn-xs" id="remove" onclick="remove(this)">' +
-                            '<input type="hidden" id="rentalInfoId" value="' + item.rentalInfoId + '">' +
-                            '<i class="layui-icon layui-icon-delete">删除</i>' +
-                            '</button>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
+                                '</li>'
+                            );
+                        });
 
-                            '</li>'
-                        );
-                    });
+                        //执行下一页渲染，第二参数为：满足“加载更多”的条件，即后面仍有分页
+                        //pages为Ajax返回的总页数，只有当前页小于总页数的情况下，才会继续出现加载更多
+                        next(lis.join(''), page < res.data.lastPage);
+                    }
+                })
+            }
+        });
+    };
 
-                    //执行下一页渲染，第二参数为：满足“加载更多”的条件，即后面仍有分页
-                    //pages为Ajax返回的总页数，只有当前页小于总页数的情况下，才会继续出现加载更多
-                    next(lis.join(''), page < res.data.lastPage);
-                }
+    //初始加载
+    pullDate();
 
+    //条件查询
+    $("#queryByCondition").on("click", function () {
+        $("#LAY_demo1").html("");
+        pullDate();
 
-            })
+    });
 
-
+    //我的
+    $("#myPost").on("click", function () {
+        $("#LAY_demo1").html("");
+        var userId=$.cookie('user_id');
+        if (userId!= null) {
+            pullDate(userId);
+        }else{
+            layer.msg("请先登录!");
         }
+
+
     });
 
 
@@ -233,6 +172,7 @@ $("#quitFun").on("click", function () {
 
 //我要发布
 $("#postRental").on("click", function () {
+
     if ($.cookie('user_id') != null) {
         $(location).attr('href', 'postRentalInfo.html');
     } else {
@@ -242,30 +182,34 @@ $("#postRental").on("click", function () {
 
 });
 
-
+//删除
 function remove(a) {
 
 
-    var questUrl = "/rentalInfo/" + a.firstElementChild.value + "/remove";
-    $.ajax({
-        url: questUrl,
-        type: "POST",
-        dataType: "json",
-        success: function (msg) {
-            if (msg.status == 0) {
-                layer.msg("删除成功");
-                a.parentElement.parentElement.parentElement.parentElement.remove();
+    layer.confirm('确认删除?', function (index) {
+        //do something
+        var questUrl = "/rentalInfo/" + a.firstElementChild.value + "/remove";
+        $.ajax({
+            url: questUrl,
+            type: "POST",
+            dataType: "json",
+            success: function (msg) {
+                if (msg.status === 0) {
 
-            } else {
-                layer.msg("删除失败");
+                    layer.msg("删除成功！");
+                    a.parentElement.parentElement.parentElement.parentElement.remove();
+
+                } else {
+                    layer.msg("删除失败！");
+                }
+
+            },
+            error: function (msg) {
+                layer.msg("删除失败！");
             }
-
-        },
-        error: function (msg) {
-            layer.msg("删除失败");
-        }
+        });
+        layer.close(index);
     });
-
 
 }
 
